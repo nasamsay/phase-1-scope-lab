27fc3a03a58b56733d68f79e8ce964173aed14e7
Nasam Sayour <nasamsay@gmail.com>
2022-08-12 00:32:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName ='bob';
+const leastFavoriteCustomer='paul';
+function upperCaseCustomerName() {
+    customerName= customerName.toUpperCase();
+    console.log(customerName)
+}
+
+
+
+console.log(bestCustomer)
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+    return bestCustomer;
+}
+console.log(setBestCustomer());
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    console.log(bestCustomer)
+}
+overwriteBestCustomer();
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer ='jack'
+    console.log(leastFavoriteCustomer);
+}
+
+console.log(changeLeastFavoriteCustomer())
